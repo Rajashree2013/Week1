2ddbcb4fa52b0d620cd966d82cea6d8f528c150e
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-23 10:07:45 +0100

    Practice program

---
+++ src/main/java/se/lexicon/rajashree/DecimalAddition.java
@@ -0,0 +1,15 @@
+package se.lexicon.rajashree;
+
+public class DecimalAddition {
+    public static void main(String args[]){
+
+        double value1 = 10.3;
+        double value2 = 5.3;
+        double sum = 0.0;
+
+        sum = value1 + value2;
+
+        System.out.println(sum);
+
+    }
+}
---
+++ src/main/java/se/lexicon/rajashree/Name.java
@@ -0,0 +1,10 @@
+package se.lexicon.rajashree;
+
+public class Name {
+    public static void main(String args[]){
+        String firstName = "Rajashree";
+        String secondName = "Bhuyan";
+
+        System.out.println("My Name :  "+firstName+ " "+ secondName);
+    }
+}
