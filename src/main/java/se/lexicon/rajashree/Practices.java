56bcfbce14ba2b5fe110ee50b141261535242e2a
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-23 09:55:46 +0100

    Print first name

---
+++ src/main/java/se/lexicon/rajashree/Practices.java
@@ -0,0 +1,10 @@
+package se.lexicon.rajashree;
+
+public class Practices {
+    public static void main(String[]args) {
+
+
+        String firstName = "Rajashree";
+        System.out.println(firstName);
+    }
+}
