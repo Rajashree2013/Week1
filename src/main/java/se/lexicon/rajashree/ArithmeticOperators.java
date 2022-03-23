2b574d24752d27266c2f1b45641d9759a7ee3cbe
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-23 16:16:34 +0100

    Arithmetic and Average file checkin

---
+++ src/main/java/se/lexicon/rajashree/ArithmeticOperators.java
@@ -0,0 +1,33 @@
+package se.lexicon.rajashree;
+
+public class ArithmeticOperators {
+
+
+public static void main(String[]args){
+
+
+
+//Addition Operation
+
+    int sum = 45 + 11;
+    System.out.println("Sum is: " + sum);
+
+//Multiplication Operation
+    int mul = 12 * 4;
+    System.out.println("Multiplied value is : " + mul);
+
+
+
+//Division Operation
+    int div = 24 / 6;
+    System.out.println("Quotient is : " + div);
+
+
+//Subtraction Operation
+    int dif = 55 - 12;
+    System.out.println("Difference is : " + dif);
+
+
+
+}
+}
---
+++ src/main/java/se/lexicon/rajashree/AverageofthreeNumbers.java
@@ -0,0 +1,36 @@
+package se.lexicon.rajashree;
+
+public class AverageofthreeNumbers {
+
+
+    public static void main(String[] args) {
+        int x;
+        int y;
+        int z;
+
+        int Sum;
+
+
+        Sum = (23 + 11 + 77) / 3;
+
+
+        System.out.print("The average value is " + Sum);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
